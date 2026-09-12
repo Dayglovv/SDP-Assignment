9eaa22fb576b692d787e189d059de81ce019d5d2
aliin <aliinfiniity@gmail.com>
2026-09-12 22:01:51 +0500

    also add CharacterBuilder

---
+++ src/Dnd/CharacterBuilder.java
@@ -0,0 +1,12 @@
+package Dnd;
+
+public interface CharacterBuilder {
+    CharacterBuilder setNickname(String nickname);
+    CharacterBuilder setrace(String race);
+    CharacterBuilder setcharaccterClass(String characterClass);
+    CharacterBuilder sethealth(int health);
+    CharacterBuilder setlevel(int level);
+    CharacterBuilder setfaith(boolean faith);
+    CharacterBuilder setweapon(String weapon);
+    Character build();
+}
